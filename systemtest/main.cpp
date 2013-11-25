697c736969a0a2ed5a5912fcdf78fcbe248fc8f6
MSchouwenburg <m.schouwenburg@utwente.nl>
2013-11-25 15:10:26 +0100

    created

---
+++ systemtest/main.cpp
@@ -0,0 +1,18 @@
+#include <QtWidgets/QApplication>
+#include <QStringList>
+#include <QTest>
+
+#include "kernel.h"
+#include "numericrange.h"
+#include "geometries.h"
+#include "ilwisdata.h"
+#include "ellipsoid.h"
+#include "geodeticdatum.h"
+#include "systemtest/systemtest.h"
+
+using namespace Ilwis;
+
+QTEST_MAIN(LoaderTests)
+
+
+
