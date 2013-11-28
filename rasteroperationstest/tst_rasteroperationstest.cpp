6fe507e9e5cd3f366b383dba23341eb997c5ffab
Henning Bredel <h.bredel@52north.org>
2013-11-28 18:29:43 +0100

    add percentage test and refactor test class

--- rasteroperationstest/tst_rasteroperationstest.cpp
+++ rasteroperationstest/tst_rasteroperationstest.cpp
@@ -23,8 +23,9 @@
 
 using namespace Ilwis;
 
-SymbolTable _symtbl;
-ExecutionContext _ctx;
+class QFileInfo;
+
+#define IMAGE_TO_STRETCH "tmb3.mpr"
 
 RasterOperationsTest::RasterOperationsTest()
 {
@@ -33,48 +34,41 @@ RasterOperationsTest::RasterOperationsTest()
 void RasterOperationsTest::initTestCase()
 {
     QUrl s = QString("file:///%1").arg(ROOT_TESTDATA);
-    mastercatalog()->addContainer(s);
     Catalog cat;
     cat.prepare(s);
     context()->setWorkingCatalog(cat);
 
+    IRasterCoverage mpl;
+    QString res = QString("file:///%1/%2").arg(ROOT_TESTDATA).arg(IMAGE_TO_STRETCH);
+    QVERIFY2(mpl.prepare(res), QString("could not prepare '%1'").arg(res).toLatin1().constData());
 }
 
 void RasterOperationsTest::cleanupTestCase()
 {
-    // TODO clean up mastercatalog etc.
 }
 
-void RasterOperationsTest::testLinearStretchOperation()
+void RasterOperationsTest::testLinearStretchWithValueLimits()
 {
     try {
-        /*
-        INumericItemDomain bitsValueDomain;
-        bitsValueDomain.prepare();
-
-        NumericItemRange bitsValueRange;
-        bitsValueRange.interpolation("");
-        bitsValueRange << "0 255 1";
-        bitsValueDomain->setRange(bitsValueRange);
-
-        _mpl->datadefIndex().domain(bitsValueDomain);
-        _mpl->setLayerIndexes(bitsValueRange);
-         */
-
-
-        IRasterCoverage mpl;
-        QString res = QString("file:///%1/small.mpl").arg(ROOT_TESTDATA);
-        QVERIFY2(mpl.prepare(res), QString("could not prepare '%1'").arg(res).toLatin1().constData());
-
-        ExecutionContext ctx;
-        Operation op({"stretch_out=linearstretch(small.mpl)"});
         qDebug() << "stretch raster";
-        QVERIFY2(op->execute(&ctx,_symtbl), "operation done.");
+        QString expr = QString("stretch_out=linearstretch(%1, 70, 120)").arg(IMAGE_TO_STRETCH);
 
-
+        Operation op(expr);
+        QVERIFY2(op->execute(&_ctx,_symtbl), "stretching with value limits failed.");
     }
-    catch (const std::length_error& le) {
-        qDebug() << "Length error: " << le.what() << '\n';
+    catch(ErrorObject& err) {
+        qDebug() << err.message();
+        QVERIFY(false);
+    }
+}
+
+void RasterOperationsTest::testLinearStretchWithPercentLimits() {
+    try {
+        qDebug() << "stretch raster";
+        QString expr = QString("stretch_out=linearstretch(%1, 10)").arg(IMAGE_TO_STRETCH);
+
+        Operation op(expr);
+        QVERIFY2(op->execute(&_ctx,_symtbl), "stretching with percent limits failed.");
     }
     catch(ErrorObject& err) {
         qDebug() << err.message();
@@ -83,5 +77,5 @@ void RasterOperationsTest::testLinearStretchOperation()
 }
 
 
-//#include "tst_rasteroperationstest.moc"
+#include "moc_tst_rasteroperationstest.cpp"
 
--- rasteroperationstest/tst_rasteroperationstest.h
+++ rasteroperationstest/tst_rasteroperationstest.h
@@ -3,6 +3,11 @@
 
 #include <QObject>
 
+#include "commandhandler.h"
+#include "symboltable.h"
+
+namespace Ilwis {
+
 class RasterOperationsTest : public QObject
 {
     Q_OBJECT
@@ -13,9 +18,16 @@ public:
 
 private Q_SLOTS:
     void initTestCase();
-    void testLinearStretchOperation();
+    void testLinearStretchWithValueLimits();
+    void testLinearStretchWithPercentLimits();
     void cleanupTestCase();
+
+
+private:
+    SymbolTable _symtbl;
+    ExecutionContext _ctx;
+
 };
 
-
+}
 #endif // TST_RASTEROPERATIONSTEST_H
