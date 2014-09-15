b6f413e6979dbeda2e780911ec4e4a37432e5d51
Martin Schouwenburg <mschouwen@gmail.com>
2014-09-15 07:17:02 +0200

    added example of subfeature use

--- CoreTest/ilwisobjects/coverage/featurecoveragetest.cpp
+++ CoreTest/ilwisobjects/coverage/featurecoveragetest.cpp
@@ -1,18 +1,9 @@
-#include "kernel.h"
-#include "testsuite.h"
-#include "ilwisdata.h"
-#include "domain.h"
-#include "datadefinition.h"
-#include "columndefinition.h"
-#include "table.h"
 #include "raster.h"
-#include "coverage.h"
+#include "testsuite.h"
+#include "table.h"
 #include "ellipsoid.h"
-#include "projection.h"
-#include "coordinatesystem.h"
-#include "attributerecord.h"
-#include "feature.h"
 #include "featurecoverage.h"
+#include "feature.h"
 #include "geometryhelper.h"
 #include "numericdomain.h"
 #include "domainitem.h"
@@ -28,6 +19,47 @@ FeatureCoverageTest::FeatureCoverageTest() : IlwisTestCase("FeatureCoverageTest"
 {
 }
 
+void FeatureCoverageTest::testVariants() {
+    Ilwis::IFeatureCoverage featureCoverage;
+    featureCoverage.prepare();
 
+    Ilwis::ICoordinateSystem csy("code=proj4:+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs");
+
+    Ilwis::Envelope env(Ilwis::LatLon(50,-30), Ilwis::LatLon(-50, 60));
+    featureCoverage->coordinateSystem(csy);
+    featureCoverage->envelope(env);
+
+    Ilwis::ITimeDomain dom;
+    dom.prepare();
+    dom->range(new Ilwis::TimeInterval("20090101", "20110101"));
+    std::vector<QString> times = {"20090101","20090131","20090602","20090703", "20100109","20110101"};
+
+    featureCoverage->attributeDefinitionsRef().addColumn("population", "value");
+    featureCoverage->attributeDefinitionsRef().setSubDefinition(dom,times);
+
+    Ilwis::SPFeatureI feature1 = featureCoverage->newFeature(0);
+
+    geos::geom::Geometry *geom = Ilwis::GeometryHelper::fromWKT("Linestring(40 20, 10 12, -20 -10)", featureCoverage->coordinateSystem().ptr());
+    feature1->geometry(geom);
+    feature1("population", 200);
+
+    geom = Ilwis::GeometryHelper::fromWKT("Linestring(30 10, 10 15, -23 -12)",featureCoverage->coordinateSystem().ptr());
+
+    feature1->createSubFeature("20090101", geom);
+
+    auto *attrdef = new Ilwis::FeatureAttributeDefinition();
+    attrdef->addColumn("temperature", "value");
+    featureCoverage->attributeDefinitionsRef().featureAttributeDefinition(attrdef);
+
+    feature1["20090101"]("temperature", 26.5);
+
+    double v = feature1["20090101"]("temperature").toDouble();
+
+
+
+
+
+
+}
 
 
